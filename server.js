65f61b51ed87970fe8163c2f72230dbceca4fc86
ParraDav <df.parra.07@gmail.com>
2026-04-27 21:56:35 -0500

    Auto stash before merge of "DavidParra" and "origin/StevenOspina"

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-
 const app = express();
 
-// Middleware
-app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("API funcionando");
+});
 
-// Rutas
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/cursos", require("./routes/cursoRoutes"));
-
-// Conexión MongoDB
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("Conectado a MongoDB");
-
-    app.listen(process.env.PORT, () => {
-        console.log("Servidor corriendo en puerto " + process.env.PORT);
-    });
-})
-.catch(err => console.log(err));
+app.listen(3000, () => {
+    console.log("Servidor en puerto 3000");
+});
